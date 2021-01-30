2768248f036b2779a1a975d5d491b7846ce6611c
Aaron Chang <xesuschang@gmail.com>
2021-01-30 23:19:38 +0800

    add program

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/jamieabc/battery-monitor
+
+go 1.14
+
+require (
+	github.com/gen2brain/beeep v0.0.0-20200526185328-e9c15c258e28 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20200217142428-fce0ec30dd00 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+)
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gen2brain/beeep"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var BattInfoPath string
+
+type Info int
+
+const (
+	CapacityInfo Info = iota
+	StatusInfo
+)
+
+var BattInfo map[Info]string
+
+type Status int
+
+const (
+	StatusCharging Status = iota
+	StatusDischarging
+)
+
+var BattStatus map[Status]string
+var DischargeLow, ChargeHigh int
+
+func init() {
+	BattInfoPath = "/sys/class/power_supply/BAT0"
+
+	BattInfo = make(map[Info]string)
+	BattInfo[CapacityInfo] = "capacity"
+	BattInfo[StatusInfo] = "status"
+
+	BattStatus = make(map[Status]string)
+	BattStatus[StatusCharging] = "Charging"
+	BattStatus[StatusDischarging] = "Discharging"
+
+	DischargeLow = 20
+	ChargeHigh = 80
+}
+
+func main() {
+	checkBatteryDir(BattInfoPath)
+
+	go periodicCheck()
+
+	for {
+	}
+}
+
+func checkBatteryDir(path string) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic(fmt.Sprintf("%s not exist", path))
+	}
+}
+
+func periodicCheck() {
+	ticker := time.NewTicker(5 * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			status := read(BattFile(BattInfoPath, BattInfo[StatusInfo]))
+			st := strings.Trim(status, "\n")
+
+			capacity := read(BattFile(BattInfoPath, BattInfo[CapacityInfo]))
+			caps, err := strconv.Atoi(strings.Trim(capacity, "\n"))
+			if err != nil {
+				panic(err)
+			}
+
+			if st == BattStatus[StatusDischarging] && caps <= DischargeLow {
+				msg := fmt.Sprintf("Low battery %d", caps)
+				err = beeep.Notify("Battery Info", msg, "")
+				if err != nil {
+					panic(err)
+				}
+			}
+
+			if st == BattStatus[StatusCharging] && caps >= ChargeHigh {
+				msg := fmt.Sprintf("Batter full %d", cap)
+				err = beeep.Notify("Battery Info", msg, "")
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+}
+
+func BattFile(dir, category string) string {
+	return fmt.Sprintf("%s/%s", dir, category)
+}
+
+func read(path string) string {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(data)
+}
